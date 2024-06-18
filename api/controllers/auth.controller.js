1ad4b4c78eb5d7c6ed827401ae6e4722b046bfc1
phanbadu <duphan03@gmail.com>
2024-06-19 04:35:52 +0700

    complete sign up and page funcionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,11 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+    if (!username || !email || !password === 0) return next(errorHandler(401, 'Vui lòng nhập đủ thông tin các trường'));
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     try {
